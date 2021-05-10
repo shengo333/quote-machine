19c9f31ecd683e5b2e60469f8a4995a82de3cffb
shengo333 <75250413+shengo333@users.noreply.github.com>
2021-05-10 08:09:18 +0400

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# quote machine
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/shengo/pen/jOBEXNY](https://codepen.io/shengo/pen/jOBEXNY).
+
+
---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = randomtext;
+function randomtext() {
+  document.getElementById("text").innerText = "some text";
+  document.getElementById("author").innerText = "some author ";
+}
+function generateQuote() {
+  console.log("Generate Quote");
+}
